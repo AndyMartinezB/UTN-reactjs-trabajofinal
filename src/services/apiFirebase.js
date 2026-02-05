a790f8c27ee5e0f23934f84ffa9ad9fb1eaf1f8b
Andy <andres.bologna@mi.unc.edu.ar>
2026-02-05 00:15:09 -0300

    feat: lógica de crud con firestore en apiFirebase.jsx. Continuar con home

---
+++ src/services/apiFirebase.js
@@ -0,0 +1,45 @@
+import {
+  addDoc,
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+
+import { db } from "../config/firebase.js";
+
+const productsCollection = collection(db, "products");
+
+const addProduct = async (product) => {
+  const docRef = await addDoc(productsCollection, product);
+  return {
+    id: docRef.id,
+    ...product,
+  };
+};
+
+const getAllProducts = async () => {
+  const snapshot = await getDocs(productsCollection);
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
+
+const updateProduct = async (id, updates) => {
+  const product = doc(db, "products", id);
+  await updateDoc(product, updates);
+  return {
+    id,
+    ...updates,
+  };
+};
+
+const deleteProduct = async (id) => {
+  const product = doc(db, "products", id);
+  await deleteDoc(product);
+  return id;
+};
+
+export { getAllProducts, addProduct, updateProduct, deleteProduct };
